bbeb54d8c9ebd0dcc2fe320f4ca9192714768e27
n8fr8 <nathan@freitas.net>
2014-01-17 21:03:16 -0500

    various build time fixes

--- fix-support-library.sh
+++ fix-support-library.sh
@@ -5,7 +5,21 @@ good_jar="app/libs/android-support-v4.jar"
 
 # all these libs depend on android-support-v4.jar
 readarray <<END
-external/HoloEverywhere/contrib/ActionBarSherlock/actionbarsherlock
+external/CacheWord/cachewordlib
+external/OnionKit/libonionkit
+END
+
+for project in "${MAPFILE[@]}"; do
+    project=${project%$'\n'} # remove trailing newline
+    echo "updating $good_jar in $project"
+    cp -f $good_jar $project/libs
+done
+
+# we must use the same support lib jar in all the dependencies
+good_jar="app/libs/sqlcipher.jar"
+
+# all these libs depend on android-support-v4.jar
+readarray <<END
 external/CacheWord/cachewordlib
 END
 
@@ -15,4 +29,16 @@ for project in "${MAPFILE[@]}"; do
     cp -f $good_jar $project/libs
 done
 
+# we must use the same support lib jar in all the dependencies
+good_jar="app/libs/iocipher.jar"
 
+# all these libs depend on android-support-v4.jar
+readarray <<END
+external/CacheWord/cachewordlib
+END
+
+for project in "${MAPFILE[@]}"; do
+    project=${project%$'\n'} # remove trailing newline
+    echo "updating $good_jar in $project"
+    cp -f $good_jar $project/libs
+done
--- setup-ant.sh
+++ setup-ant.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 projectname=`sed -n 's,.*name="app_name">\(.*\)<.*,\1,p' app/res/values/strings.xml`
+target="android-19"
 
 echo "Setting up build for $projectname"
 echo ""
@@ -8,6 +9,6 @@ echo ""
 for f in `find external/ -name project.properties`; do
 projectdir=`dirname $f`
     echo "Updating ant setup in $projectdir:"
-    android update lib-project -p $projectdir
+    android update lib-project -p $projectdir -t $target
 done
-android update project -p app/ --subprojects --name "$projectname"
+android update project -p app/ --subprojects --name "$projectname" --target $target -s
