7d4d68d2710e3e8e806707b92716d608eb51546f
Jason Dartmouth <jason.dartmouth@yahoo.com>
2014-01-25 16:46:19 -0500

    Made MacOS X compatible

--- fix-support-library.sh
+++ fix-support-library.sh
@@ -4,15 +4,24 @@
 good_jar="app/libs/android-support-v4.jar"
 
 # all these libs depend on android-support-v4.jar
-readarray <<END
-external/HoloEverywhere/contrib/ActionBarSherlock/actionbarsherlock
-external/CacheWord/cachewordlib
-END
+#mapfile <<END # requires newer bash than on MacOS X
+#external/ActionBarSherlock/actionbarsherlock
+#external/CacheWord/cachewordlib
+#END
+
+MAPFILE[0]='external/ActionBarSherlock/actionbarsherlock'
+MAPFILE[1]='external/CacheWord/cachewordlib'
+
 
 for project in "${MAPFILE[@]}"; do
     project=${project%$'\n'} # remove trailing newline
     echo "updating $good_jar in $project"
     cp -f $good_jar $project/libs
 done
+
+rm external/bho/TbChat/libs/guava-r09.jar
+rm external/CacheWord/cachewordlib/libs/guava-r09.jar
+cp external/securereaderlibrary/libs/guava-11.0.2.jar external/bho/TbChat/libs/
+cp external/securereaderlibrary/libs/guava-11.0.2.jar external/CacheWord/cachewordlib/libs/
 
 
